74fd8be60f288c9c912ce734b10cda7de7024ea1
Lei Su <sulei@uniontech.com>
2021-01-05 16:47:47 +0800

    refactor(multitasking): Add the 'WindowInfo' class for passing parameters between QML and C++.
    Log: Add the 'WindowInfo' class for passing parameters between QML and C++.
    
    Change-Id: I16e36a6ad9a5729f470555632088d9c0c5965413

---
+++ plugins/kwineffects/multitasking/windowinfo.cpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2020 Uniontech Technology Co., Ltd.
+ *
+ * Author:     Lei Su <sulei@uniontech.com>
+ *
+ * Maintainer: Lei Su <sulei@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "windowinfo.h"
+
+WindowInfo::WindowInfo(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+WindowInfo::WindowInfo(const WindowInfo &other)
+{
+    operator=(other);
+}
+
+WindowInfo &WindowInfo::operator=(const WindowInfo &other)
+{
+    if (this != &other) {
+        m_windowId    = other.m_windowId;
+        m_windowTitle = other.m_windowTitle;
+        m_windowIcon  = other.m_windowIcon;
+    }
+
+    return (*this);
+}
+
+void WindowInfo::setWindowId(const QVariant &winId)
+{
+    if (m_windowId != winId) {
+        m_windowId = winId;
+
+        emit windowIdChanged(m_windowId);
+    }
+}
+
+QVariant WindowInfo::windowId() const
+{
+    return m_windowId;
+}
+
+void WindowInfo::setWindowTitle(const QString &windowTitle)
+{
+    if (m_windowTitle != windowTitle) {
+        m_windowTitle = windowTitle;
+
+        emit windowTitleChanged(m_windowTitle);
+    }
+}
+
+QString WindowInfo::windowTitle() const
+{
+    return m_windowTitle;
+}
+
+void WindowInfo::setWindowIcon(const QIcon &windowIcon)
+{
+    m_windowIcon = windowIcon;
+
+    emit windowIconChanged(m_windowIcon);
+}
+
+QIcon WindowInfo::windowIcon() const
+{
+    return m_windowIcon;
+}
---
+++ plugins/kwineffects/multitasking/windowinfo.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 Uniontech Technology Co., Ltd.
+ *
+ * Author:     Lei Su <sulei@uniontech.com>
+ *
+ * Maintainer: Lei Su <sulei@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef _WINDOW_INFO_H_
+#define _WINDOW_INFO_H_
+
+#include <QIcon>
+#include <QObject>
+#include <QVariant>
+
+class WindowInfo : public QObject
+{
+    Q_OBJECT
+public:
+    explicit WindowInfo(QObject *prarent = nullptr);
+    WindowInfo(const WindowInfo &other);
+    WindowInfo &operator= (const WindowInfo &other);
+
+    void setWindowId(const QVariant &winId);
+    QVariant windowId() const;
+
+    void setWindowTitle(const QString &windowTitle);
+    QString windowTitle() const;
+
+    void setWindowIcon(const QIcon &windowIcon);
+    QIcon windowIcon() const;
+
+signals:
+    void windowIdChanged(QVariant winId);
+    void windowTitleChanged(QString windowTitle);
+    void windowIconChanged(QIcon windowIcon);
+
+private:
+    QVariant m_windowId;
+    QString  m_windowTitle;
+    QIcon    m_windowIcon;
+};
+
+Q_DECLARE_METATYPE(WindowInfo)
+
+#endif //_WINDOW_INFO_H_
