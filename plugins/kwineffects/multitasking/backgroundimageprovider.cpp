f4506ab731d25fe2d9ade07bda9de749c2b26f86
zhangyu <zhangyud@uniontech.com>
2021-07-12 19:15:10 +0800

    fix: reuse thumbnail cache
    reuse thumbnail cache
    
    Log: reuse thumbnail cache
    
    Bug: https://pms.uniontech.com/zentao/bug-view-86606.html
    
    Change-Id: Ia27db6ff65bb51ad73c6581884850de194c60f3b

--- plugins/kwineffects/multitasking/backgroundimageprovider.cpp
+++ plugins/kwineffects/multitasking/backgroundimageprovider.cpp
@@ -16,10 +16,10 @@
 #include <X11/Xatom.h>
 #include <QX11Info>
 
+static XImage *pImage = nullptr;
 BackgroundImageProvider::BackgroundImageProvider(QQmlImageProviderBase::ImageType type, QQmlImageProviderBase::Flags flags)
     : QQuickImageProvider(type, flags)
 {
-
 }
 
 QImage BackgroundImageProvider::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
@@ -38,7 +38,10 @@ QImage BackgroundImageProvider::requestImage(const QString &id, QSize *size, con
     unsigned unusedUint, width, height;
 
     XGetGeometry(display, winId, &unusedWindow, &unusedInt, &unusedInt, &width, &height, &unusedUint, &unusedUint);
-    XImage *pImage = XGetImage(display, winId, 0, 0, width, height, AllPlanes, ZPixmap);
+    if (pImage == nullptr)
+    	pImage = XGetImage(display, winId, 0, 0, width, height, AllPlanes, ZPixmap);
+    else
+	pImage = XGetSubImage(display, winId, 0, 0, width, height, AllPlanes, ZPixmap, pImage, 0, 0);
 
     QImage image = QImage((const uchar *)(pImage->data), pImage->width, pImage->height, pImage->bytes_per_line, QImage::Format_RGB32);
     image = image.convertToFormat(QImage::Format_ARGB32);
